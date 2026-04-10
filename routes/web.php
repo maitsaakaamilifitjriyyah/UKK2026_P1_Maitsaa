a33df41d80b7b6db42484645ba146e7b6c5971e3
maitsaakaamilifitjriyyah <maitsaakaamilif@gmail.com>
2026-04-10 18:59:18 +0700

    category location

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,10 +28,35 @@ Route::middleware('auth')->group(function () {
 });
 
 // User
-Route::get('/user', [UserController::class, 'index'])->name('user.show');
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
 Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
 Route::post('/user', [UserController::class, 'store'])->name('user.store');
 Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
 Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
 Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+// Category
+Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
+Route::get('/category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
+Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+
+// Location
+Route::get('/location', [LocationController::class, 'index'])->name('location.index');
+Route::get('/location/create', [LocationController::class, 'create'])->name('location.create');
+Route::post('/location', [LocationController::class, 'store'])->name('location.store');
+Route::get('/location/{id}/edit', [LocationController::class, 'edit'])->name('location.edit');
+Route::put('/location/{id}', [LocationController::class, 'update'])->name('location.update');
+Route::delete('/location/{id}', [LocationController::class, 'destroy'])->name('location.destroy');
+
+// Item
+// Route::get('/item', [ItemController::class, 'index'])->name('item.index');
+// Route::get('/item/create', [ItemController::class, 'create'])->name('item.create');
+// Route::post('/item', [ItemController::class, 'store'])->name('item.store');
+// Route::get('/item/{id}/edit', [ItemController::class, 'edit'])->name('item.edit');
+// Route::put('/item/{id}', [ItemController::class, 'update'])->name('item.update');
+// Route::delete('/item/{id}', [ItemController::class, 'destroy'])->name('item.destroy');
+// Route::get('/item/{id}', [ItemController::class, 'detail'])->name('item.detail');
 require __DIR__.'/auth.php';
